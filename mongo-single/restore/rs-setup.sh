d1369a0f1dd97bc7ad6a848258700387056e488a
296204 <296204@ho.bigc.co.th>
2022-09-20 19:39:36 +0700

    update

--- mongo-single/restore/rs-setup.sh
+++ mongo-single/restore/rs-setup.sh
@@ -3,5 +3,4 @@
 mongoimport --db bl --collection user --file user
 mongoimport --db bl --collection bank --file bank
 mongoimport --db bl --collection role --file role
-mongoimport --db bl --collection tReport --file tReport
 mongoimport --db bl --collection basicContent --file basicContent.json --jsonArray
